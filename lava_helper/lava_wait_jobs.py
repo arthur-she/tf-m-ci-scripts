9f7469b78ef9faee3d40615af97de251942ffc01
Antonio de Angelis <Antonio.deAngelis@arm.com>
2024-04-20 00:32:31 +0100

    lava_wait_jobs: Use format to avoid specifying type
    The print for timeout expiring is using the
    wrong format specifier. Just use the format
    method on strings when printing to avoid
    specifying at all.
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: Ida8e5a625fd764afe0f1bfdee6c11cbefa340e51

--- lava_helper/lava_wait_jobs.py
+++ lava_helper/lava_wait_jobs.py
@@ -67,7 +67,7 @@ def get_finished_jobs(job_list, user_args, lava):
     finished_jobs = lava.block_wait_for_jobs(job_list, user_args.dispatch_timeout, 5, callback=inline_fetch_artifacts)
     unfinished_jobs = [item for item in job_list if item not in finished_jobs]
     for job in unfinished_jobs:
-        _log.info("Cancelling unfinished job %d because of timeout.", job)
+        _log.info("Cancelling unfinished job {} because of timeout.".format(job))
         lava.cancel_job(job)
     if len(unfinished_jobs) > 0:
         _log.info("Job fails because some test jobs have been cancelled.")
