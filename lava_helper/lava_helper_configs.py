cee9f696d4630985cf5303415a7e67ff703dcc56
Arthur She <arthur.she@linaro.org>
2024-06-10 19:20:00 +0200

    Revert "LAVA: Disable b_u585i_iot02a platform temporary"
    This reverts commit 542569841f1e11080d4a7f39b9f1e4ea7ef3ba00.
    
    Reason for revert: The issue has been fixed by the following patch.
    Patch: https://review.trustedfirmware.org/c/TF-M/trusted-firmware-m/+/29034/1..2
    Test jib: https://ci.trustedfirmware.org/view/TF-M/job/tf-m-build-and-test/10229/console
    
    Signed-off-by: Arthur She <arthur.she@linaro.org>
    Change-Id: I1b4d9e6d698a412d9e081b1d7428cd527a3f66ab

--- lava_helper/lava_helper_configs.py
+++ lava_helper/lava_helper_configs.py
@@ -374,7 +374,7 @@ lava_gen_config_map_bl2 = {
     "qemu_mps2_bl2": qemu_mps2_bl2,
     "musca_b1": musca_b1_bl2,
     "stm32l562e_dk": stm32l562e_dk,
-# disable platform temporarily being unstable    "b_u585i_iot02a": b_u585i_iot02a,
+    "b_u585i_iot02a": b_u585i_iot02a,
     "stm32h573i_dk": stm32h573i_dk
 }
 
