0d1086110182433fb140bfea3e8687358feadddd
Matthew Dalzell <matthew.dalzell@arm.com>
2024-06-21 21:12:06 +0100

    Removes profile small from Misra configs
    Change-Id: If47b92c1d4cc88fa9c1dc8863e1bf854dc63e7cf
    Signed-off-by: Matthew Dalzell <matthew.dalzell@arm.com>

--- build_helper/build_helper_configs.py
+++ build_helper/build_helper_configs.py
@@ -681,7 +681,7 @@ config_misra = {"seed_params": {
                 "test_psa_api":     ["OFF"],
                 "cmake_build_type": ["Debug"],
                 "with_bl2":         [True],
-                "profile":          ["profile_small", "profile_medium_arotless"],
+                "profile":          ["profile_medium_arotless"],
                 "extra_params":     ["PSOFF"]
                 },
                 "common_params": _common_tfm_builder_cfg,
