5cf10c67ef16d0812067663708e6a61b3b18681b
Matthew Dalzell <matthew.dalzell@arm.com>
2024-09-19 11:22:48 +0100

    CONFIGS: Switches CS1000 to release build only
    Change-Id: I273640a08db4fba222138e31b02fe398572ac3f8
    Signed-off-by: Matthew Dalzell <matthew.dalzell@arm.com>

--- build_helper/build_helper_configs.py
+++ build_helper/build_helper_configs.py
@@ -993,7 +993,7 @@ config_corstone1000 = {"seed_params": {
                 "isolation_level":  ["1", "2"],
                 "test_regression":  ["RegS"],
                 "test_psa_api":     ["OFF"],
-                "cmake_build_type": ["Debug"],
+                "cmake_build_type": ["Release"],    # previously Debug
                 "with_bl2":         [True],
                 "profile":          [""],
                 "extra_params":     ["NSOFF, CS1K_TEST, FVP", "NSOFF, CS1K_TEST, FPGA"]
