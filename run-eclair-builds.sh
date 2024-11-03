a689858df5ea12f7b7e4bd386c8f5ab927b98ace
Antonio de Angelis <antonio.deangelis@arm.com>
2024-11-03 19:49:44 +0000

    Eclair-builds: Modify git commands to checkout index and clean
    Use git checkout-index to checkout the index into the working
    tree (forced, for all files, quiet). Expand on using git clean
    to remove all files in the working tree which are not in the
    index (forced, for all files and directories, quiet). This
    should fixed the recent issue with eclair-daily builds due to
    the new RPI2350 patch on psa-arch-tests fetch_repo().
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    Change-Id: I85c325fcaa1363d76c7fccf8a197efa58e1e9dab

--- run-eclair-builds.sh
+++ run-eclair-builds.sh
@@ -16,9 +16,9 @@ cnt=1
 for cfg in $(python3 ./tf-m-ci-scripts/configs.py -g "$FILTER_GROUP"); do
     echo "============== $cfg ($cnt/$num_configs) =============="
     export CONFIG_NAME=$cfg
-    (cd mbedtls; git checkout .; git clean -fq)
-    (cd psa-arch-tests; git checkout .; git clean -fq)
-    (cd trusted-firmware-m; git checkout .; git clean -fq)
+    (cd mbedtls; git checkout-index -faq; git clean -fdq)
+    (cd psa-arch-tests; git checkout-index -faq; git clean -fdq)
+    (cd trusted-firmware-m; git checkout-index -faq; git clean -fdq)
     eclair_tfm_set_toolchain_path
     detachLicense $LICENSE_LEASE
     tf-m-ci-scripts/run-build.sh
